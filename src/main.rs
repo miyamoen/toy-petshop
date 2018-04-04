f7970230911bd766a2d36c23f380a544f9b730b2
miyamoen <akihito.biblioteca@gmail.com>
2018-04-04 22:46:16 +0900

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate simplelog;
 extern crate log;
 use simplelog::*;
 use std::fs::File;
+extern crate dotenv;
 
 extern crate futures;
 extern crate gotham;
@@ -21,9 +22,25 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+use dotenv::dotenv;
+use std::env;
+
 use router::router;
 
 pub fn main() {
+    log_init();
+    dotenv().ok();
+
+    let addr: &str = &env::var("ADDRES").unwrap_or("0.0.0.0".to_string());
+    let port: u16 = env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(7878);
+    println!("Listening for requests at http://{}", addr);
+    gotham::start((addr, port), router())
+}
+
+fn log_init() {
     CombinedLogger::init(vec![
         TermLogger::new(LevelFilter::Warn, Config::default()).unwrap(),
         WriteLogger::new(
@@ -32,8 +49,4 @@ pub fn main() {
             File::create("my_rust_binary.log").unwrap(),
         ),
     ]).unwrap();
-
-    let addr = "127.0.0.1:7878";
-    println!("Listening for requests at http://{}", addr);
-    gotham::start(addr, router())
 }
