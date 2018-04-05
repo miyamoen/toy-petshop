6a3f44b731eb229813596e8e84ceb95383939831
miyamoen <akihito.biblioteca@gmail.com>
2018-04-05 22:39:44 +0900

    diesel getting started

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod handlers;
 mod middleware;
 mod model;
+mod my_diesel;
 mod router;
+mod schema;
 
 extern crate simplelog;
 #[macro_use]
@@ -22,14 +24,34 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+#[macro_use]
+extern crate diesel;
 use dotenv::dotenv;
 use std::env;
 
+use diesel::prelude::*;
+use my_diesel::establish_connection;
 use router::router;
 
 pub fn main() {
     log_init();
     dotenv().ok();
+
+    use schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<model::Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
 
     let addr: &str = &env::var("ADDRES").unwrap_or("0.0.0.0".to_string());
     let port: u16 = env::var("PORT")
--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,1 +1,9 @@
 pub mod pet;
+
+#[derive(Queryable)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
